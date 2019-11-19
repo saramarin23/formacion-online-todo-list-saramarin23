a94d93d4302f2096db39558f52fc296270428d2d
saramarin23 <saramarin.econ@gmail.com>
2019-11-20 00:23:13 +0100

    add main style, add button adds

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 "use strict";
 
-console.log("Holi");
+const addButton = document.querySelector("#add-btn");
+
+function addDate() {
+  const numberDate = document.querySelector(".day-container");
+  const stringDate = document.querySelector(".string-container");
+  console.log(stringDate);
+
+  let dayToday = new Date().toLocaleDateString("es-ES", { day: "numeric" });
+  let weekToday = new Date().toLocaleDateString("es-ES", { weekday: "long" });
+  let monthYearToday = new Date().toLocaleDateString("es-ES", {
+    month: "long",
+    year: "numeric"
+  });
+  stringDate.innerHTML = weekToday;
+}
+
+function addTask(e) {
+  e.preventDefault();
+  const list = document.querySelector(".list-container");
+  console.log(list);
+  list.innerHTML += `<li class="list-item"><input id="checkbox" type="checkbox" /><p id="task-text"></p></li>`;
+  console.log("Has clickao");
+}
+
+window.addEventListener("load", addDate);
+addButton.addEventListener("click", addTask);
