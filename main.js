907024f012bc816490c5a0b37485f4763ccc94be
saramarin23 <saramarin.econ@gmail.com>
2019-11-20 11:42:22 +0100

    add the tasks I write

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const addButton = document.querySelector("#add-btn");
+const mainContainer = document.querySelector(".main-container");
+const resetLogo = document.querySelector("#logo");
 
 function addDate() {
   const numberDate = document.querySelector(".day-container");
@@ -13,16 +15,41 @@ function addDate() {
     month: "long",
     year: "numeric"
   });
-  stringDate.innerHTML = weekToday;
+  //   stringDate.innerHTML = weekToday;
 }
 
-function addTask(e) {
-  e.preventDefault();
-  const list = document.querySelector(".list-container");
-  console.log(list);
-  list.innerHTML += `<li class="list-item"><input id="checkbox" type="checkbox" /><p id="task-text"></p></li>`;
-  console.log("Has clickao");
+function addContainerTask() {
+  mainContainer.innerHTML += `<section class="add-task-section"><div class="add-task-popup">
+    <span class="new-task">Nueva tarea</span>
+    <input class="add-task-name" type="text" />
+    <button class="add-task-btn">Añadir</button></div></section>`;
+  const addTaskBtn = document.querySelector(".add-task-btn");
+
+  const addTaskSection = document.querySelector(".add-task-section");
+  addTaskSection.classList.add("hidden");
+  addTaskBtn.addEventListener("click", addTask);
 }
+
+function addTask() {
+  const addTaskSection = document.querySelector(".add-task-section");
+  addTaskSection.classList.remove("hidden");
+  const list = document.querySelector(".list-container");
+  const taskName = document.querySelector(".add-task-name").value;
+  console.log(taskName);
+  list.innerHTML += `<li class="list-item"><input id="checkbox" type="checkbox" /><p id="task-text">${taskName}</p></li>`;
+  //   addContainerTask();
+}
+
+// function hidePopUp() {
+//   const addTaskSection = document.querySelector(".add-task-section");
+//   addTaskSection.classList.remove("hidden");
+//   console.log(event.target);
+// }
 
 window.addEventListener("load", addDate);
-addButton.addEventListener("click", addTask);
+addButton.addEventListener("click", function() {
+  addContainerTask();
+  addTask();
+});
+
+// resetLogo.addEventListener("click", hidePopUp);
